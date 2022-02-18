2dacaa1de308a365056004f712342a2e257f2253
Acer <Acer@DESKTOP-F9ONNJE>
2022-02-18 19:27:46 +0530

    interfaces

---
+++ interfaces/src/interfaces/Interfaceprogram.java
@@ -0,0 +1,52 @@
+package interfaces;
+
+
+	
+
+	interface A{
+	 void happy();	
+	}
+
+	interface B{
+		 void sad();	
+	}
+
+	interface C{
+		 void hungry();	
+	}
+	 
+	interface D{
+		void sleep();
+		
+	}
+
+	class Emotions implements A,B,C,D{
+	public void happy() {
+	 System.out.println("1.Iam Happy");	
+	}	
+
+
+	public void sad() {
+		 System.out.println("2.I Feel Sad");	
+		}	
+	public void hungry() {
+		 System.out.println("3.Iam Hungry");	
+		}	
+	public void sleep() {
+		 System.out.println("4.Take a nap");	
+		}
+
+	}
+
+	public class  Interfaceprogram{
+		public static void main(String args[]) {
+			   Emotions e= new Emotions();
+			   e.happy();
+			   e.sad();
+			   e.hungry();
+			   e.sleep();
+	        }
+	}
+	
+
+
